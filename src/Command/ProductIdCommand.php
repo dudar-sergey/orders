44e4baa8bd5efd42ebb05c545abd5ad4b64966ba
Sergey <dudar.sr@yandex.ru>
2021-05-24 17:35:48 +0300

    add cheto

--- src/Command/ProductIdCommand.php
+++ src/Command/ProductIdCommand.php
@@ -49,9 +49,10 @@ class ProductIdCommand extends Command
         $io->progressStart(count($products));
         foreach ($products as $product) {
             $data = json_decode($this->am->getProductId($product->getUpc(), $profile), true);
-            if(isset($data['products'][0]))
-            $product->setAllegroProductId($data['products'][0]['id']);
-            $product->setAllegroImages($data['products'][0]['images']);
+            if(isset($data['products'][0])) {
+                $product->setAllegroProductId($data['products'][0]['id']);
+                $product->setAllegroImages($data['products'][0]['images']);
+            }
             $io->progressAdvance();
         }
         $io->progressFinish();
