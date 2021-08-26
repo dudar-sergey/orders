46da05bbb82fe7412d407df6e66ec3dfaaa5c55a
Sergey <dudar.sr@yandex.ru>
2021-08-26 15:43:40 +0300

    fix

--- src/Command/CronCommand.php
+++ src/Command/CronCommand.php
@@ -2,7 +2,9 @@
 
 namespace App\Command;
 
+use App\ebay\AllegroUserManager;
 use App\Entity\CronLog;
+use App\Entity\Profile;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -10,17 +12,23 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CronCommand extends Command
 {
     protected static $defaultName = 'cron';
     protected static $defaultDescription = 'Add a short description for your command';
     private $em;
+    private $authAllegro;
+    private $profileRep;
+    private $am;
 
-    public function __construct(EntityManagerInterface $em, string $name = null)
+    public function __construct(EntityManagerInterface $em, AllegroUserManager $am, string $name = null)
     {
         parent::__construct($name);
         $this->em = $em;
+        $this->profileRep = $em->getRepository(Profile::class);
+        $this->am = $am;
     }
 
     protected function configure()
@@ -41,6 +49,16 @@ class CronCommand extends Command
         $cronLog->setCreateAt(new \DateTime('now', new \DateTimeZone('Europe/Moscow')));
         $this->em->persist($cronLog);
         $this->em->flush();
+        /** @var Profile[] $profiles */
+        $profiles = $this->profileRep->findAllAllegro();
+        foreach ($profiles as $profile) {
+            try {
+                $tokens = $this->am->getTokenForUserWithRefreshToken($profile);
+                $profile->setAllegroAccessToken($tokens['accessToken']);
+                $profile->setAllegroRefreshToken($tokens['refreshToken']);
+                $this->em->flush();
+            } catch (\Exception $e) {}
+        }
         return Command::SUCCESS;
     }
 }
